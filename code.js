d28733cc3f5f3ce7d1ffc859a1051b989e6a19a5
Jason Casares <jasonc@csdrafting.com>
2016-07-06 00:00:53 +0000

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(NAME_OF_ICECREAM) {
+  return "I love " + NAME_OF_ICECREAM;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+    return true;
+}
